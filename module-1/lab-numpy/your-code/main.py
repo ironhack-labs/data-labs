3d5d6e3eff94a1c723df48772e81a3965a9f8984
Zhou Zhou <zhou.eye8@gmail.com>
2018-10-18 17:34:18 -0400

    numpy lab solution

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,85 +1,117 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print np.version.version
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.random((2, 3, 5))
+#or
+a = np.random.rand(2, 3, 5)
+#or
+a = np.empty((2,3,5))
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5, 2, 3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+a.size == b.size
+"""
+>>> a.size == b.size
+True
+"""
 
 
 #8. Are you able to add a and b? Why or why not?
+a+b
+"""
+>>> a+b
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3)
 
+a and b cannot be added because they have different shapes
+"""
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = np.transpose(b, (1, 2, 0))
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = a + c
+#It works now because a and c have the same shape.
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
+print(a)
+print(b)
+"""
+Each value in d is larger than the corresponding value in a by 1. This is because all the values in c are 1. 
+When you add a and c, the values at the same position get added together resulting in the corresponding value in d.
+"""
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = a*c
 
 
 #13. Does e equal to a? Why or why not?
+"""
+>>> e == a
+array([[[ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True]],
 
+       [[ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True]]])
 
+e equals to a. When you multiply a and c, the values at the same position get multiplied. 
+Because all values in c are 1, each value in e equals to the corresponding value in a.
+"""
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
+d_max = np.amax(d)
+d_min = np.amin(d)
+d_mean = np.mean(d)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+f = np.empty((2, 3, 5))
 
 
-
-
-"""
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
-If a value equals to d_mean, assign 50 to the corresponding value in f.
-Assign 0 to the corresponding value(s) in f for d_min in d.
-Assign 100 to the corresponding value(s) in f for d_max in d.
-In the end, f should have only the following values: 0, 25, 50, 75, and 100.
-Note: you don't have to use Numpy in this question.
-"""
+# If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+# If a value equals to d_mean, assign 50 to the corresponding value in f.
+# Assign 0 to the corresponding value(s) in f for d_min in d.
+# Assign 100 to the corresponding value(s) in f for d_max in d.
+# In the end, f should have only the following values: 0, 25, 50, 75, and 100.
+# Note: you don't have to use Numpy in this question.
+f[d<d_mean]=25
+f[d>d_mean]=75
+f[d==d_min]=0
+f[d==d_mean]=50
+f[d==d_max]=100
 
 
-
-
-"""
 #17. Print d and f. Do you have your expected f?
+"""
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
@@ -98,8 +130,6 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
-
 
 
 """
