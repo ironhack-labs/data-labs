83283d1c64bb3fa732a6dce9c6e86aae344d711c
Elena JP <elenajp@MacBook-Air-de-Elena-2.local>
2018-10-26 09:37:53 +0200

    main.py

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -8,44 +8,52 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
-
+import pandas as pd
+import numpy as np
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+square = [i**2 for i in range(20)]
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+power_of_two = [i**2 for i in range(50)]
+print (power_of_two)
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
+sqrt = [i**(1/2) for i in range(100)]
+print (sqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
 
-
+my_list = [-i for i in range(11)]
+my_list.reverse()
+print (my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
-
+odds = [i for i in range(100) if i % 2!=0 ]
+print (odds)
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99]
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+divisible_by_seven = [i for i in range(1,1000) if i % 7 == 0]
+print(divisible_by_seven)
 
+[7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 126, 133, 140, 147, 154, 161, 168, 175, 182, 189, 196, 203, 210, 217, 224, 231, 238, 245, 252, 259, 266, 273, 280, 287, 294, 301, 308, 315, 322, 329, 336, 343, 350, 357, 364, 371, 378, 385, 392, 399, 406, 413, 420, 427, 434, 441, 448, 455, 462, 469, 476, 483, 490, 497, 504, 511, 518, 525, 532, 539, 546, 553, 560, 567, 574, 581, 588, 595, 602, 609, 616, 623, 630, 637, 644, 651, 658, 665, 672, 679, 686, 693, 700, 707, 714, 721, 728, 735, 742, 749, 756, 763, 770, 777, 784, 791, 798, 805, 812, 819, 826, 833, 840, 847, 854, 861, 868, 875, 882, 889, 896, 903, 910, 917, 924, 931, 938, 945, 952, 959, 966, 973, 980, 987, 994]
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -54,21 +62,27 @@ print(my_listComprehension)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
-
+non_vowels = "".join ([k for k in teststring if k not in vowels])
+ 
+non_vowels
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
-
+text = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+capital_letters = ''.join([k for k in text if k in 'ABCDEFGHIJKLMNOPQRSTOVWXYZ'])
+capital_letters
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
-
+text = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+consonants = ''.join([k for k in text if k in 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTUVWXYZ'])
+consonants
 
 
 
@@ -76,13 +90,36 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+import os 
+patch='/Users/elenajp/desktop/Ironhack/madrid-oct-2018'
+files = [p for p in os.listdir(patch)]
+display (files)
 
+['.DS_Store',
+ '.git',
+ '.gitignore',
+ 'final-project',
+ 'images',
+ 'LICENSE',
+ 'module-1',
+ 'module-2',
+ 'module-3',
+ 'README.md']
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+import random
+my_randoms = random.sample(range(101), 10)
+for i in range(4):
+    print (my_randoms)
 
+
+    [45, 44, 75, 66, 57, 99, 67, 9, 26, 43]
+[45, 44, 75, 66, 57, 99, 67, 9, 26, 43]
+[45, 44, 75, 66, 57, 99, 67, 9, 26, 43]
+[45, 44, 75, 66, 57, 99, 67, 9, 26, 43]
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
@@ -90,7 +127,10 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 
-
+list_of_lists = [[1,2,3], [4,5,6], [7,8,9]]
+flatten_list = [val for sublist in list_of_lists for val in sublist]
+flatten_list
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -107,6 +147,13 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 
 for i in ['a','b','c']:
     print i**2
+
+   try: 
+    for i in ['a','b','c']:
+        print(i**2)
+except TypeError as e: 
+    print("Error detectado en EXCEPTION")
+     
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
